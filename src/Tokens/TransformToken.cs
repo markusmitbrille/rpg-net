b93c9a6f96055eb58ce81533c5b49c6a8fae3c60
Markus Rezai <markus.rezai@gmail.com>
2018-02-19 13:55:58 +0100

    New serialization made it possible to directly serialize Vector3

--- src/Tokens/TransformToken.cs
+++ src/Tokens/TransformToken.cs
@@ -6,52 +6,23 @@ using UnityEngine;
 public class TransformToken : MonoBehaviour
 {
     [DataMember]
-    private Vector3Token Position
+    private Vector3 Position
     {
-        get { return new Vector3Token(transform.localPosition); }
-        set { transform.localPosition = value.ToVector3(); }
+        get { return transform.localPosition; }
+        set { transform.localPosition = value; }
     }
 
     [DataMember]
-    private Vector3Token Rotation
+    private Vector3 Rotation
     {
-        get { return new Vector3Token(transform.localEulerAngles); }
-        set { transform.localEulerAngles = value.ToVector3(); }
+        get { return transform.localEulerAngles; }
+        set { transform.localEulerAngles = value; }
     }
 
     [DataMember]
-    private Vector3Token Scale
+    private Vector3 Scale
     {
-        get { return new Vector3Token(transform.localScale); }
-        set { transform.localScale = value.ToVector3(); }
-    }
-
-    [DataContract]
-    private struct Vector3Token
-    {
-        [DataMember]
-        private float x;
-
-        [DataMember]
-        private float y;
-
-        [DataMember]
-        private float z;
-
-        public Vector3Token(Vector3 vector3)
-        {
-            x = vector3.x;
-            y = vector3.y;
-            z = vector3.z;
-        }
-
-        public Vector3 ToVector3()
-        {
-            Vector3 result;
-            result.x = x;
-            result.y = y;
-            result.z = z;
-            return result;
-        }
+        get { return transform.localScale; }
+        set { transform.localScale = value; }
     }
 }
