c0e7f9da87a758599a728d36d26ba480b5052a2f
Markus Rezai <markus.rezai@gmail.com>
2018-02-22 11:50:24 +0100

    Added skeleton for player actor
    To have a non abstract actor for actor inspector tests

---
+++ src/Auras/PlayerActor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerActor : Actor
+{
+    public override Actor Target
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public override Vector3 Aim
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
